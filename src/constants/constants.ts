54a4aaf92eda9c4c5b3a385c5841e0e431939934
shree-2802 <pattupadmashree@gmail.com>
2024-01-24 10:09:40 +0530

    constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,14 @@
+export const ArrayOfObjects = [
+  {
+    first: 'Shivan',
+    last: 'Parvathi',
+  },
+  {
+    first: 'Parvathi',
+    last: 'Shivan',
+  },
+  {
+    first: 'Ganesh ashokaSundari Murugan',
+    last: 'shivan',
+  },
+];

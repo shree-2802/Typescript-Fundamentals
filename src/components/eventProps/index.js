2cfdef7754c74c392b9c6dda110318fd6fb202b7
shree-2802 <pattupadmashree@gmail.com>
2024-01-24 21:50:06 +0530

    js

---
+++ src/components/eventProps/index.js
@@ -0,0 +1,4 @@
+import ButtonEventSender from './buttonEvent/buttonEventSender';
+import InputPropsSender from './inputEvent/inputPropsSender';
+
+export { ButtonEventSender as ButtonSender, InputPropsSender as InputProp };

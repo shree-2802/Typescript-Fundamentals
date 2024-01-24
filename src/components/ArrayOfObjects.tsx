f86394dd6b62b3fa4603556b5eef832f96e8e96a
shree-2802 <pattupadmashree@gmail.com>
2024-01-24 10:09:32 +0530

    ObjectArray

---
+++ src/components/ArrayOfObjects.tsx
@@ -0,0 +1,7 @@
+import { ArrayOfObjectsType } from '../types/types';
+const ArrayOfObjects = ({ names }: ArrayOfObjectsType) => {
+  console.log(names);
+  return <div>ArrayOfObjects</div>;
+};
+
+export default ArrayOfObjects;

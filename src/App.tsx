b0a37f200ef817e7adf30932e132612a7c0a2bb7
shree-2802 <pattupadmashree@gmail.com>
2024-01-24 10:09:17 +0530

    tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { ArrayOfObjects } from './constants/constants';
+import ArrayOfObjects1 from './components/ArrayOfObjects';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ArrayOfObjects1 names={ArrayOfObjects} />
     </div>
   );
 }
